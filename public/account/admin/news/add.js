e712dca94b898d690111346016c36522dff068af
Mr-Offline <arsalan13812002@gmail.com>
2020-10-19 15:37:49 +0330

    Update add.js

--- public/account/admin/news/add.js
+++ public/account/admin/news/add.js
@@ -2,9 +2,8 @@ $(document).ready(function(){
     $(document).on('submit','form',function(e)
     {
         e.preventDefault();
-        ajaxHandler($(this),false).done(function(data)
+        ajaxHandler($(this)).done(function(data)
         {
-            ajaxT.html(data)
             data=$.parseJSON(data)
             validationMessage(false,data.msg,data.type,data.err,'.validation-message')
             if(data.err==null) setTimeout(function(){
