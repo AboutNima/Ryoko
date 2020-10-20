35c89a9f0ea657b0a8bf0b11b286323c12f80b0a
Mr-Offline <arsalan13812002@gmail.com>
2020-10-20 12:06:07 +0330

    edit news

--- app/controller/account/admin/news/edit.php
+++ app/controller/account/admin/news/edit.php
@@ -3,8 +3,9 @@ $data=$db->where('id',$id)->objectBuilder()->getOne('News',[
 	'title','link','demo','description','keywords','UNIX_TIMESTAMP(archiveDate) as archiveDate'
 ]);
 if(empty($data)){
-	die(header('location:/account/news/list'));
+	die(header('location:/404'));
 }
+$_SESSION['DATA']['News']['EDIT']['ID']=$id;
 $urlCrt[2]=$data->title;
 $script='/public/account/admin/news/add';
 require_once 'app/controller/motherPage/adminHeader.php';
